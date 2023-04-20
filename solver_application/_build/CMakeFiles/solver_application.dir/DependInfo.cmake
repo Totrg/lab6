83409f6ccdcf9b6ec2980635bab0346921974141
Totrg <belogolov2005@mail.ru>
2023-04-20 12:27:06 +0300

    initial

---
+++ solver_application/_build/CMakeFiles/solver_application.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/totrg/formatter/lab03/solver_application/equation.cpp" "CMakeFiles/solver_application.dir/equation.cpp.o" "gcc" "CMakeFiles/solver_application.dir/equation.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/totrg/formatter/lab03/solver_application/_build/CMakeFiles/formatter_ex.dir/DependInfo.cmake"
+  "/home/totrg/formatter/lab03/solver_application/_build/CMakeFiles/solver_lib.dir/DependInfo.cmake"
+  "/home/totrg/formatter/lab03/solver_application/_build/CMakeFiles/formatter_lib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
